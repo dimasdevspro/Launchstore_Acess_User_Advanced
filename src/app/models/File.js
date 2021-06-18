21c2a90b35f6e4725e5e1f7b21e116766efeb0f0
dimasdevspro <dimasdevspro@gmail.com>
2021-06-18 16:44:41 -0300

    cleaning the code by comments

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -5,20 +5,3 @@ Base.init({table: 'files'})
 module.exports = {
     ...Base,
 }
-//     async delete(id) {
-
-//         try{
-//             const results = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
-//             const file = results.rows[0]
-            
-//             fs.unlinkSync(file.path)
-            
-//          }catch(err){
-//                 console.error(err)
-//             }
-
-//         return db.query(`
-//         DELETE FROM files WHERE Id = $1
-//         `)
-//     }
-// }
